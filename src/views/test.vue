2f6befd4939ff081fa7952aba22b8c815fcd693a
mijinB <bmj13465@gmail.com>
2023-06-29 16:56:23 -0700

    quasar css

--- src/views/test.vue
+++ src/views/test.vue
@@ -17,32 +17,34 @@ export default {
     <div class="layout">
         <div class="aside">
             <img src="@/assets/images/logo.png" />
-            <div id="primaryTheme">Bmijin</div>
+            <div class="text-h5 text-weight-bold text-dark">Bmijin</div>
             <div class="asideText">ddd</div>
             <div class="asideText">fff</div>
         </div>
         <div class="main">
-            <div class="mainLayout">
-                <div class="todayDate">{{ year }}년 {{ month }}월 {{ day }}일</div>
-                <div class="cards">
+            <div>
+                <div class="text-h5 q-pl-xl q-pt-md text-weight-bold text-white">{{ year }}년 {{ month }}월 {{ day }}일</div>
+                <div>
                     <!--카드-->
 
-                    <q-card flat bordered class="my-card">
+                    <q-card flat bordered class="my-card q-ma-xl bg-warning">
                         <q-card-section>
-                            <div class="text-h7">주차장 1 </div>
+                            <div class="text-h6 text-weight-bold text-dark">주차장 1 </div>
                         </q-card-section>
 
                         <q-separator dark inset />
 
-                        <q-card-section class="text-subtitle2">
+                        <q-card-section class="text-subtitle2 text-dark">
                             담당 전화번호: 010-1234-5678<br />
                             주소 : 전라북도 전주시<br />
                             (신주소 : ㅇㅇ로)<br />
-                            <div class="countBox">
-                                <div class="card">
-                                    <a style="border-bottom: 1px solid red;">주차 차량 수</a>
+                            <div>
+                                <div>
+                                    주차 차량 수
+                                    <q-separator dark inset />
+                                    dd
                                 </div>
-                                <div class="card">
+                                <div>
                                     잔여 공간 수
                                 </div>
                             </div>
@@ -67,41 +69,28 @@ export default {
         align-items: center;
         width: 200px;
         padding: 20px;
-        background-color: #364153;
+        background-color: #F2C037;
     }
     .asideText {
         align-self: flex-start;
-        color: #CFD9E0;
         font-size: 15px;
     }
     .main {
         display: flex;
         flex: 1;
         flex-wrap: wrap;
-        background-color: #1A202C;
+        background-color: #1d1d1d;
     }
-    .mainLayout {
+    /* .mainLayout {
         display:flex;
         flex-direction: column
-    }
-    .todayDate {
-        padding-top: 20px;
-        padding-left: 110px;
-        color: white;
-        font-size: 25px;
-        font-weight: 600;
-    }
-    .cards {
+    } */
+    /* .cards {
         display:flex;
         flex-wrap: wrap;
         justify-content: space-evenly;
-    }
-    #primaryTheme {
-        color: #605CFF;
-        font-size: 25px;
-        font-weight: 600;
-    }
-    .countBox {
+    } */
+    /* .countBox {
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
@@ -113,17 +102,5 @@ export default {
         width: 90px;
         background-color: #605CFF;
         color: white;
-    }
-
-    .my-card {
-        margin: 60px;
-        border-radius: 12px;
-        background-color: #364153;
-        color: white;
-        font-size: 18px;
-        font-weight: 600;
-    }
-    .text-h7 {
-        color: #605CFF;
-    }
+    } */
 </style>
