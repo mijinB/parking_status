c32ce9be9c4f87f3709f4502ff80f87a25cce6ab
mijinB <bmj13465@gmail.com>
2023-07-03 18:56:59 -0700

    Card Component size & Button create

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <div class="q-pa-md text-center">
+      <q-btn-dropdown
+        color="dark"
+        rounded
+        label="Parking"
+      >
+        <q-list>
+          <q-item clickable v-close-popup class="bg-dark">
+
+            <q-item-section>
+              <q-item-label class="text-white">dd</q-item-label>
+            </q-item-section>
+            
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
+  </template>
--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,46 +1,14 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import axios from 'axios';
-
-const carParkUrl = "http://192.168.1.82:1880/config";
-let parkList = ref([]);
-
-// onMounted(() => {
-//     axios
-//         .get(carParkUrl)
-//         .then(res => {
-//             console.log(res.data.edge[0].parking_config[0].area);
-//             parkList.value = res.data.edge[0].parking_config[0].area
-//                 .map(item => {
-//                     return {
-//                         id: item.area_id,
-//                         title: item.area_name
-//                     }
-//                 })
-//         })
-//         .catch(err => console(err));
-// })
-const titleValue = ref('');
-
 const props = defineProps({
-    id: Number
-})
-
-onMounted(() => {
-    axios
-        .get(carParkUrl)
-        .then(res => {
-            titleValue.value = res.data.edge[0].parking_config[0].area[props.id].area_name;
-        })
-        .catch(err => console(err));
+    parkName: String
 })
 </script>
 
 <template>
-    <div class="bg-center" style="width: 100vh;">
+    <div class="float-left">
         <q-card flat bordered class="my-card q-ma-xl bg-warning">
             <q-card-section>
-                <div class="text-h6 text-dark text-weight-bold">{{ titleValue }}</div>
+                <div class="text-h6 text-dark text-weight-bold">{{ props.parkName }}</div>
             </q-card-section>
 
             <q-separator inset />
@@ -51,16 +19,19 @@ onMounted(() => {
                 (신주소 : ㅇㅇ로)
                 <div class="text-subtitle2 q-mt-sm text-dark text-weight-bold text-right">주차면수 : ㅇㅇㅇ</div>
                 <div class="row justify-between">
-                    <q-card flat bordered class="my-card q-pa-xs q-mt-xs bg-dark">
-                        <div class="text-subtitle2 text-warning text-weight-bold">주차 차량 수</div>
+
+                    <q-card flat bordered class="my-card col q-pa-xs q-ma-xs bg-dark">
+                        <div class="text-subtitle2 text-warning text-weight-bold text-center">주차 차량 수</div>
                         <q-separator dark />
                         <div class="text-subtitle2 text-warning text-weight-bold text-right">ㅇㅇ</div>
                     </q-card>
-                    <q-card flat bordered class="my-card q-pa-xs q-mt-xs bg-dark">
-                        <div class="text-subtitle2 text-warning text-weight-bold">주차 가능 수</div>
+
+                    <q-card flat bordered class="my-card col q-pa-xs q-ma-xs bg-dark">
+                        <div class="text-subtitle2 text-warning text-weight-bold text-center">주차 가능 수</div>
                         <q-separator dark />
                         <div class="text-subtitle2 text-warning text-weight-bold text-right">ㅇㅇㅇ</div>
                     </q-card>
+
                 </div>
             </q-card-section>
             <div class="text-subtitle text-dark text-center">1년 누적 주차대수 : ㅇㅇㅇㅇㅇ</div>
